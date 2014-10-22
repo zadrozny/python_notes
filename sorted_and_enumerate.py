714bdf35cf1dc2714ca2f59051506288da7086fd
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-22 11:19:55 -0400

    Strange behavior when mixing sorted and enumerate

---
+++ sorted_and_enumerate.py
@@ -0,0 +1,19 @@
+#THE CONJUNCTION OF SORTED AND ENUMERATE:
+
+#Start with an unsorted list:
+lst = (1, 2, 1)
+
+#Iterating over it, the order is preserved
+for element in lst: 
+	print element
+print 
+
+#Sorting then iterating, we get the sorted order
+for element in sorted(lst): 
+	print element
+print
+
+#However if we both sort and enumerate, we get the new order, 
+#while the indices continue to point to the original list. Strange.
+for i, element in enumerate(sorted(lst)): 
+	print element, lst[i]
