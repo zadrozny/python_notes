60163d6c5f3dbbf3151e9bd1919a4862c6e7f377
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-11-15 21:26:00 -0500

    Cleaned up comments

--- sorted_and_enumerate.py
+++ sorted_and_enumerate.py
@@ -1,19 +1,20 @@
-#THE CONJUNCTION OF SORTED AND ENUMERATE:
+# THE CONJUNCTION OF SORTED AND ENUMERATE
 
-#Start with an unsorted list:
+# Start with an unsorted list:
 lst = (1, 2, 1)
 
-#Iterating over it, the order is preserved
+
+# Iterating over it, the order is preserved
 for element in lst: 
 	print element
-print 
 
-#Sorting then iterating, we get the sorted order
+
+# Sorting then iterating, we get the sorted order
 for element in sorted(lst): 
 	print element
-print
 
-#However if we both sort and enumerate, we get the new order, 
-#while the indices continue to point to the original list. Strange.
+
+# However if we both sort and enumerate, we get the new order, 
+# while the indices continue to point to the original list. Strange.
 for i, element in enumerate(sorted(lst)): 
 	print element, lst[i]
