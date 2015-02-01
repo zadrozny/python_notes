3695afb8a0bfe876a0517388f9c4942776740056
MZ <m.zadrozny@gmail.com>
2015-01-31 19:08:41 -0500

    An example of method resolution order in inheritance.

---
+++ method_resolution_order.py
@@ -0,0 +1,43 @@
+#!/usr/lib64/python3.3
+
+# https://github.com/akulakov
+
+# Method resolution order in Python:
+# 	https://www.python.org/download/releases/2.3/mro/
+
+
+# Compare this...
+class A:
+    def x(self):
+        print("in A")
+        super().x()
+
+class B:
+    def x(self):
+        print("in B")
+
+class C(A,B):
+    def x(self):
+        print("in C")
+        super().x()
+
+C().x()
+
+
+# ... with this:
+
+# class A:
+#     def x(self):
+#         print("in A")
+#         super().x()
+
+# class B:
+#     def x(self):
+#         print("in B")
+
+# class C(B,A):
+#     def x(self):
+#         print("in C")
+#         super().x()
+
+# C().x()
